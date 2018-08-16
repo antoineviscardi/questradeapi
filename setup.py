f60394e3248e09194222600ca48b25e70b12121e
avis <antoine.viscardi@gmail.com>
2018-08-16 17:51:03 -0400

    updated link to rtd

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='questradeapi',
-    version='0.0.8',
+    version='0.0.9',
     author='Antoine Viscardi',
     author_email='antoine.viscardi@gmail.com',
     description='Python wrapper for the Questrade API',
